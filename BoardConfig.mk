dae448fbe74708a522736455eeeff0caf1920562
alk3pInjection <webmaster@raspii.tech>
2022-01-16 14:52:45 +0800

    guacamole: Initial bring-up for Project ICE
     * Use qti_kernel_headers
     * move to common QTI components
     * add device fcm
     * flags for CAF platform & -ify
    
    Change-Id: Ie20e89309d705be1ffefb734b0d4cc2b08e7d989

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -41,6 +41,7 @@ BOARD_KERNEL_SEPARATED_DTBO := true
 BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOT_HEADER_VERSION)
 TARGET_KERNEL_CONFIG := vendor/sm8150-perf_defconfig
 TARGET_KERNEL_SOURCE := kernel/oneplus/sm8150
+TARGET_KERNEL_VERSION := 4.14
 
 # Platform
 BOARD_USES_QCOM_HARDWARE := true
@@ -100,11 +101,9 @@ TARGET_USES_ION := true
 # DRM
 TARGET_ENABLE_MEDIADRM_64 := true
 
-# Filesystem
-TARGET_FS_CONFIG_GEN := $(DEVICE_PATH)/config.fs
-
 # HIDL
-DEVICE_MATRIX_FILE := $(DEVICE_PATH)/compatibility_matrix.xml
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += $(DEVICE_PATH)/device_framework_matrix.xml
+DEVICE_MATRIX_FILE := device/qcom/common/compatibility_matrix.xml
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
 ODM_MANIFEST_FILES := $(DEVICE_PATH)/manifest-qva.xml
 
@@ -143,6 +142,7 @@ TARGET_USERIMAGES_USE_F2FS := true
 ENABLE_VENDOR_RIL_SERVICE := true
 
 # Security patch level
+PLATFORM_SECURITY_PATCH := 2022-04-01
 VENDOR_SECURITY_PATCH := 2022-04-01
 
 # SELinux
