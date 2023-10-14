80b889497f3d5c932c0733f99480fda168e8861a
redznn <wandersonrodriguesf1@gmail.com>
2023-10-14 00:59:25 +0000

    guacamole: Build QTI systemhelper AIDL interface
    Change-Id: I87c7289077b61d595b0b09e6fd5ac49b454d563a
    Signed-off-by: redznn <wandersonrodriguesf1@gmail.com>

--- device.mk
+++ device.mk
@@ -439,6 +439,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     vendor.qti.hardware.servicetracker@1.0.vendor
 
+# Systemhelper
+PRODUCT_PACKAGES += \
+    vendor.qti.hardware.systemhelperaidl-V1-ndk.vendor
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
